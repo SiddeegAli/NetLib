193bbefe0f45718fece6bdb32af6b9c1845701c2
SiddeegAli <siddeegalihassan@gmail.com>
2026-02-07 10:28:20 +0200

    created the base socket wraper

---
+++ include/sokcet.h
@@ -0,0 +1,33 @@
+#include "platform.h"
+
+namespace Siddeeg{
+    namespace Net{
+        class Socket{
+        public:
+            Socket(socket_t socket=invalid_socket);
+            virtual ~Socket();
+            bool init();
+            void close();
+            bool valid() const;
+
+            // DELETE Copying
+            Socket(const Socket&) = delete;
+            Socket& operator=(const Socket&) = delete;
+
+            // ENABLE Moving
+            Socket(Socket&& other) noexcept : m_socket(other.m_socket) {
+                other.m_socket = invalid_socket; // Steal the handle, leave other empty
+            }
+            Socket& operator=(Socket&& other) noexcept {
+                if (this != &other) {
+                    if (m_socket != invalid_socket) ::closesocket(m_socket); // Clean up current
+                    m_socket = other.m_socket; // Steal handle
+                    other.m_socket = invalid_socket; // Nullify source
+                }
+                return *this;
+            }
+        protected:
+            socket_t m_socket;
+        };
+    }
+}
---
+++ src/Socket.cpp
@@ -0,0 +1,23 @@
+#include<sokcet.h>
+
+Siddeeg::Net::Socket::Socket(socket_t socket):m_socket(socket){}
+
+bool Siddeeg::Net::Socket::init(){
+    m_socket = socket(AF_INET, SOCK_STREAM, 0);
+    return m_socket != invalid_socket;
+}
+
+bool Siddeeg::Net::Socket::valid() const{
+    return m_socket != invalid_socket;
+}
+
+void Siddeeg::Net::Socket::close(){
+    if(valid()){
+        ::closesocket(m_socket);
+        m_socket = invalid_socket;
+    }
+}
+
+Siddeeg::Net::Socket::~Socket(){
+    close();
+}
